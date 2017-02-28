3e035a025596de08f3d648784c1e2c08637c2c20
parkinwu <snowy_way@163.com>
2017-02-28 12:03:22 +0800

    complete 1.33

--- ex1.33.rkt
+++ ex1.33.rkt
@@ -25,4 +25,22 @@
 (accumulate1 odd? + 0 id 1 inc 10)
 (accumulate odd? + 0 id 1 inc 10)
 
+;; a)
+(define (prime? x) #t) ;; test, avoid error
+(accumulate prime? + 0 id 1 inc 10)
+;; b)
+(define (gcd-acc n)
+  (define (gcd a b)
+    (if (= b 0)
+        a
+        (gcd b (remainder a b))))
+  
+  (define (gcd-and-lt? x) (and (< x n)
+                               (= (gcd x n) 1)))
+  (accumulate gcd-and-lt? * 1 id 1 inc 10))
+
+;; test
+(gcd-acc 5) ;; 24
+
+
 
